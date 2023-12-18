6734bb402606aca07f1cd4b5bbeebc6a2af3ef9e
Artem Kulikov <chigar2010@yandex.ru>
2023-12-19 01:39:10 +0200

    add models Category, Genre, Title

--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
-# Create your models here.
+
+class Category(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+
+class Genre(models.Model):
+    name = models.CharField(max_length=256)
+    slug = models.SlugField(max_length=50, unique=True)
+
+
+class Title(models.Model):
+    name = models.CharField(max_length=256)
+    year = models.IntegerField
+    description = models.TextField(null=True, blank=True)
+    genre = models.ManyToManyField(Genre, related_name='titles')
+    category = models.ForeignKey(
+        Category, on_delete=models.SET_DEFAULT, default=None,
+        related_name='titles'
+    )

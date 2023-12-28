aa210e49d9566346ce0c50ea5e2f84d0eae5457a
Artem Kulikov <chigar2010@yandex.ru>
2023-12-28 23:32:59 +0200

    случайно написал с двойными ковычками

--- api_yamdb/api/utils.py
+++ api_yamdb/api/utils.py
@@ -7,8 +7,8 @@ from dotenv import load_dotenv
 
 load_dotenv()
 
-sender_email = os.getenv("SENDER_EMAIL")
-password = os.getenv("PASSWORD")
+sender_email = os.getenv('SENDER_EMAIL')
+password = os.getenv('PASSWORD')
 
 
 def generate_verification_code():
@@ -17,12 +17,12 @@ def generate_verification_code():
 
 def send_verification_email(recipient_email, verification_code):
     msg = EmailMessage()
-    msg["Subject"] = "Код подтверждения"
-    msg["From"] = sender_email
-    msg["To"] = recipient_email
-    msg.set_content(f"Ваш код подтверждения: {verification_code}")
+    msg['Subject'] = 'Код подтверждения'
+    msg['From'] = sender_email
+    msg['To'] = recipient_email
+    msg.set_content(f'Ваш код подтверждения: {verification_code}')
 
-    with smtplib.SMTP_SSL("smtp.yandex.ru", 465) as server:
+    with smtplib.SMTP_SSL('smtp.yandex.ru', 465) as server:
         server.login(sender_email, password)
         server.send_message(msg)
         server.quit()

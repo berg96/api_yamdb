64142259abda2fc871402c8f2b59f59290f6e74d
Artem Kulikov <chigar2010@yandex.ru>
2024-01-14 19:11:37 +0200

    fix

--- api_yamdb/api_yamdb/settings.py
+++ api_yamdb/api_yamdb/settings.py
@@ -21,9 +21,9 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'api.apps.ApiConfig',
-    'reviews.apps.ReviewsConfig',
     'rest_framework',
+    'api',
+    'reviews',
     'rest_framework_simplejwt',
     'django_filters',
 ]

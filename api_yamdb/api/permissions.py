5548f0a7feba55bdc6a5a21d8142895333f02c76
Artem Kulikov <chigar2010@yandex.ru>
2024-01-15 23:48:44 +0200

    micro fix

--- api_yamdb/api/permissions.py
+++ api_yamdb/api/permissions.py
@@ -7,7 +7,7 @@ class IsAdminUserOrReadOnly(permissions.BasePermission):
     def has_permission(self, request, view):
         return (request.method in permissions.SAFE_METHODS
                 or request.user.is_authenticated
-                and request.user.role == ADMIN)
+                and (request.user.role == ADMIN or request.user.is_superuser))
 
 
 class IsAuthorAdminModeratorOrReadOnlyPermission(permissions.BasePermission):
@@ -22,7 +22,8 @@ class IsAuthorAdminModeratorOrReadOnlyPermission(permissions.BasePermission):
 
     def has_object_permission(self, request, view, obj):
         return (request.method in permissions.SAFE_METHODS
-                or (request.user.role == ADMIN
+                or (request.user.is_superuser
+                    or request.user.role == ADMIN
                     or request.user.role == MODERATOR
                     or obj.author == request.user))
 
